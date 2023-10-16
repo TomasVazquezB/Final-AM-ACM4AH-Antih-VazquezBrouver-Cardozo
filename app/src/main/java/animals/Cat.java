cf661c5730e6b8aea54b1ed503fcb8ba5608a264
Tomas Vazquez Brouver <vbtomas@hotmail.com>
2023-10-16 12:11:05 -0300

    Codigo
    Inicio Codigo 16/10/2023

---
+++ app/src/main/java/animals/Cat.java
@@ -0,0 +1,12 @@
+package animals;
+
+public class Cat extends Animal{
+    public Cat (String name, int age, int color, int weight) {
+
+    }
+
+    public String greet() {
+
+        return null;
+    }
+}
---
+++ app/src/main/java/animals/Dog.java
@@ -0,0 +1,8 @@
+package animals;
+
+public class Dog extends Animal{
+    public Dog (String name, int age, int color, int weight) {
+
+    }
+
+}
