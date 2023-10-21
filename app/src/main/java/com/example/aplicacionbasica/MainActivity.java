9e50ef1ba0f5bc622fc05e63d38838e96b380aeb
antih <lucas.antih@davinci.edu.ar>
2023-10-21 14:16:43 -0300

    actualizacion 21/10
    arreglo de icono de aplicacion y funcionamiento correcto del programa.

---
+++ app/src/main/java/com/example/aplicacionbasica/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.aplicacionbasica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View button) {
+                LinearLayout content = findViewById(R.id.linearlayout);
+                TextView newText = new TextView(button.getContext());
+                newText.setText(R.string.textItem);
+                content.addView(newText);
+
+            }
+        });
+    }
+
+}
